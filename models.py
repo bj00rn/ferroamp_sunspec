15eda7ce3052ce7c7f8abce8521b2477bc10a11d
Björn Dalfors <bjorn@binovi.se>
2025-04-04 10:36:40 +0200

    add not implemented values for optional fields when value is None

--- models.py
+++ models.py
@@ -2,13 +2,31 @@ import logging
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import ClassVar, List
+from enum import IntEnum
 
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadBuilder
 
 log = logging.getLogger(__name__)  # Create a logger instance
 
-
+class NumberEnum(IntEnum):
+    NotImplementedUint16 = 0xFFFF
+    NotImplementedUint32 = 0xFFFFFFFF
+    NotImplementedFloat32 = 0x7FC00000  # NaN in IEEE 754
+    NotImplementedInt16 = 0x7FFF
+    NotImplementedInt32 = 0x7FFFFFFF
+    NotImplementedInt8 = 0x7F
+    NotImplementedUint8 = 0xFF
+    NotImplementedInt64 = 0x7FFFFFFFFFFFFFFF
+    NotImplementedUint64 = 0xFFFFFFFFFFFFFFFF
+    NotImplementedFloat64 = 0x7FF8000000000000  # NaN in IEEE 754
+    NotImplementedString = "\x00" * 32  # Placeholder for string fields
+    NotImplementedBitfield32 = 0xFFFFFFFF
+    NotImplementedBitfield16 = 0xFFFF
+    NotImplementedBitfield8 = 0xFF
+    NotImplementedBitfield4 = 0xF
+    NotImplementedBitfield2 = 0x3
+    NotImplementedBitfield1 = 0x1
 @dataclass
 class DataModel(ABC):
     """
@@ -114,39 +132,44 @@ class EndModel(DataModel):
 
 @dataclass
 class Model113(DataModel):
-    ID: ClassVar[int] = 113  # Model ID for Three-Phase Inverter
-    L: ClassVar[int] = 60  # Length of the Model 113 Block (in registers)
-    A: float = 0  # AC Current (A)
-    AphA: float = 0  # Phase A Current (A)
-    AphB: float = 0  # Phase B Current (A)
-    AphC: float = 0  # Phase C Current (A)
-    PPVphAB: float = 0  # Line-to-Line Voltage AB (V)
-    PPVphBC: float = 0  # Line-to-Line Voltage BC (V)
-    PPVphCA: float = 0  # Line-to-Line Voltage CA (V)
-    PhVphA: float = 0  # Line-to-Neutral Voltage AN (V)
-    PhVphB: float = 0  # Line-to-Neutral Voltage BN (V)
-    PhVphC: float = 0  # Line-to-Neutral Voltage CN (V)
-    W: float = 0  # AC Power (W)
-    Hz: float = 0  # AC Frequency (Hz)
-    VA: float = 0  # Apparent Power (VA)
-    VAr: float = 0  # Reactive Power (VAR)
-    PF: float = 0  # Power Factor
-    WH: float = 0  # AC Energy (Wh)
-    DCA: float = 0  # DC Current (A)
-    DCV: float = 0  # DC Voltage (V)
-    DCW: float = 0  # DC Power (W)
-    TmpCab: float = 0  # Cabinet Temperature (°C)
-    TmpSnk: float = 0  # Heat Sink Temperature (°C)
-    TmpTrns: float = 0  # Transformer Temperature (°C)
-    TmpOt: float = 0  # Other Temperature (°C)
-    St: int = 0  # Operating State (enum16)
-    StVnd: int = 0  # Vendor-specific Operating State (enum16)
-    Evt1: int = 0  # Event Flags 1 (bitfield32)
-    Evt2: int = 0  # Event Flags 2 (bitfield32)
-    EvtVnd1: int = 0  # Vendor-specific Event Flags 1 (bitfield32)
-    EvtVnd2: int = 0  # Vendor-specific Event Flags 2 (bitfield32)
-    EvtVnd3: int = 0  # Vendor-specific Event Flags 3 (bitfield32)
-    EvtVnd4: int = 0  # Vendor-specific Event Flags 4 (bitfield32)
+    # Class variable
+    ID: ClassVar[int] = 113  # Model ID for Three-Phase Inverter (mandatory)
+    L: ClassVar[int] = 60  # Length of the Model 113 Block (in registers) (mandatory)
+
+    # Mandatory fields
+    A: float = 0  # AC Current (A) (mandatory, float32)
+    AphA: float = 0  # Phase A Current (A) (mandatory, float32)
+    AphB: float = 0  # Phase B Current (A) (mandatory, float32)
+    AphC: float = 0  # Phase C Current (A) (mandatory, float32)
+    PPVphAB: float = 0  # Line-to-Line Voltage AB (V) (mandatory, float32)
+    PPVphBC: float = 0  # Line-to-Line Voltage BC (V) (mandatory, float32)
+    PPVphCA: float = 0  # Line-to-Line Voltage CA (V) (mandatory, float32)
+    PhVphA: float = 0  # Line-to-Neutral Voltage AN (V) (mandatory, float32)
+    PhVphB: float = 0  # Line-to-Neutral Voltage BN (V) (mandatory, float32)
+    PhVphC: float = 0  # Line-to-Neutral Voltage CN (V) (mandatory, float32)
+    W: float = 0  # AC Power (W) (mandatory, float32)
+    Hz: float = 0  # AC Frequency (Hz) (mandatory, float32)
+    VA: float = 0  # Apparent Power (VA) (mandatory, float32)
+    VAr: float = 0  # Reactive Power (VAR) (mandatory, float32)
+    PF: float = 0  # Power Factor (mandatory, float32)
+    WH: float = 0  # AC Energy (Wh) (mandatory, float32)
+    DCA: float = 0  # DC Current (A) (mandatory, float32)
+    DCV: float = 0  # DC Voltage (V) (mandatory, float32)
+    DCW: float = 0  # DC Power (W) (mandatory, float32)
+    TmpCab: float = 0  # Cabinet Temperature (°C) (mandatory, float32)
+    TmpSnk: float = 0  # Heat Sink Temperature (°C) (mandatory, float32)
+    St: int = 0  # Operating State (mandatory, enum16)
+    Evt1: int = 0  # Event Flags 1 (mandatory, bitfield32)
+    Evt2: int = 0  # Event Flags 2 (mandatory, bitfield32)
+
+    # Optional fields
+    TmpTrns: float = None  # Transformer Temperature (°C) (optional, float32)
+    TmpOt: float = None  # Other Temperature (°C) (optional, float32)
+    StVnd: int = None  # Vendor-specific Operating State (optional, enum16)
+    EvtVnd1: int = None  # Vendor-specific Event Flags 1 (optional, bitfield32)
+    EvtVnd2: int = None  # Vendor-specific Event Flags 2 (optional, bitfield32)
+    EvtVnd3: int = None  # Vendor-specific Event Flags 3 (optional, bitfield32)
+    EvtVnd4: int = None  # Vendor-specific Event Flags 4 (optional, bitfield32)
 
     def get_register(self) -> list:
         """
@@ -154,46 +177,67 @@ class Model113(DataModel):
         """
         builder = BinaryPayloadBuilder(byteorder=Endian.BIG, wordorder=Endian.BIG)
 
+        # Helper for adding 32-bit float fields
+        def add_float32(value):
+            if value is None:
+                builder.add_32bit_uint(NumberEnum.NotImplementedFloat32)  # NaN in IEEE 754
+            else:
+                builder.add_32bit_float(value)
+
+        # Helper for adding 32-bit uint fields
+        def add_uint32(value):
+            if value is None:
+                builder.add_32bit_uint(NumberEnum.NotImplementedUint32)
+            else:
+                builder.add_32bit_uint(value)
+
+        # Helper for adding 16-bit uint fields
+        def add_uint16(value):
+            if value is None:
+                builder.add_16bit_uint(NumberEnum.NotImplementedUint16)
+            else:
+                builder.add_16bit_uint(value)
+
         # Add Model ID and Length
-        builder.add_16bit_uint(self.ID)  # Model ID
-        builder.add_16bit_uint(self.L)  # Length of the Model 113 Block (in registers)
+        add_uint16(self.ID)  # Model ID
+        add_uint16(self.L)  # Length of the Model 113 Block (in registers)
 
         # Add float32 fields
-        builder.add_32bit_float(self.A)
-        builder.add_32bit_float(self.AphA)
-        builder.add_32bit_float(self.AphB)
-        builder.add_32bit_float(self.AphC)
-        builder.add_32bit_float(self.PPVphAB)
-        builder.add_32bit_float(self.PPVphBC)
-        builder.add_32bit_float(self.PPVphCA)
-        builder.add_32bit_float(self.PhVphA)
-        builder.add_32bit_float(self.PhVphB)
-        builder.add_32bit_float(self.PhVphC)
-        builder.add_32bit_float(self.W)
-        builder.add_32bit_float(self.Hz)
-        builder.add_32bit_float(self.VA)
-        builder.add_32bit_float(self.VAr)
-        builder.add_32bit_float(self.PF)
-        builder.add_32bit_float(self.WH)
-        builder.add_32bit_float(self.DCA)
-        builder.add_32bit_float(self.DCV)
-        builder.add_32bit_float(self.DCW)
-        builder.add_32bit_float(self.TmpCab)
-        builder.add_32bit_float(self.TmpSnk)
-        builder.add_32bit_float(self.TmpTrns)
-        builder.add_32bit_float(self.TmpOt)
+        add_float32(self.A)
+        add_float32(self.AphA)
+        add_float32(self.AphB)
+        add_float32(self.AphC)
+        add_float32(self.PPVphAB)
+        add_float32(self.PPVphBC)
+        add_float32(self.PPVphCA)
+        add_float32(self.PhVphA)
+        add_float32(self.PhVphB)
+        add_float32(self.PhVphC)
+        add_float32(self.W)
+        add_float32(self.Hz)
+        add_float32(self.VA)
+        add_float32(self.VAr)
+        add_float32(self.PF)
+        add_float32(self.WH)
+        add_float32(self.DCA)
+        add_float32(self.DCV)
+        add_float32(self.DCW)
+        add_float32(self.TmpCab)
+        add_float32(self.TmpSnk)
+        add_float32(self.TmpTrns)
+        add_float32(self.TmpOt)
 
         # Add enum16 fields
-        builder.add_16bit_uint(self.St)
-        builder.add_16bit_uint(self.StVnd)
+        add_uint16(self.St)
+        add_uint16(self.StVnd)
 
         # Add bitfield32 fields
-        builder.add_32bit_uint(self.Evt1)
-        builder.add_32bit_uint(self.Evt2)
-        builder.add_32bit_uint(self.EvtVnd1)
-        builder.add_32bit_uint(self.EvtVnd2)
-        builder.add_32bit_uint(self.EvtVnd3)
-        builder.add_32bit_uint(self.EvtVnd4)
+        add_uint32(self.Evt1)
+        add_uint32(self.Evt2)
+        add_uint32(self.EvtVnd1)
+        add_uint32(self.EvtVnd2)
+        add_uint32(self.EvtVnd3)
+        add_uint32(self.EvtVnd4)
 
         # Convert the payload to a list of registers
         data = builder.to_registers()
